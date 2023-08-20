a7e7b4dee9119f0c5f2433c40ae9d335ce84f134
Brian Mermel <bmermel@gmail.com>
2023-08-20 20:54:47 -0300

    tercer

--- proyectoFinal/src/main/java/com/digitalhouse/proyectoFinal/Modelo/Usuario.java
+++ proyectoFinal/src/main/java/com/digitalhouse/proyectoFinal/Modelo/Usuario.java
@@ -1,4 +1,17 @@
 package com.digitalhouse.proyectoFinal.Modelo;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Usuario {
+    private String usuario;
+    private String contrasena;
+    private String rol;
+    private Paciente paciente;
 }
