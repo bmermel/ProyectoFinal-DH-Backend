c9bf09b6e8e179072af0c6ae7547be3faad521ea
Brian Mermel <bmermel@gmail.com>
2023-08-19 22:19:40 -0300

    sgundo commit

--- proyectoFinal/src/main/java/com/digitalhouse/proyectoFinal/Modelo/Odontologo.java
+++ proyectoFinal/src/main/java/com/digitalhouse/proyectoFinal/Modelo/Odontologo.java
@@ -1,4 +1,18 @@
 package com.digitalhouse.proyectoFinal.Modelo;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Odontologo {
+    private int matricula;
+    private String nombre;
+    private String apellido;
+
 }
--- proyectoFinal/src/main/java/com/digitalhouse/proyectoFinal/Modelo/Paciente.java
+++ proyectoFinal/src/main/java/com/digitalhouse/proyectoFinal/Modelo/Paciente.java
@@ -1,4 +1,20 @@
 package com.digitalhouse.proyectoFinal.Modelo;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Paciente {
+    private int dni;
+    private String nombre;
+    private String apellido;
+    private String domicilio;
+    private Date fechaAlta;
 }
