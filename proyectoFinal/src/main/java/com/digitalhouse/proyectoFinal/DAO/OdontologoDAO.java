bb9b720ad701dc267dd748409312f82063fb4788
FJCon <conilfernando@gmail.com>
2023-08-21 21:02:05 -0300

    clases DTO

--- proyectoFinal/src/main/java/com/digitalhouse/proyectoFinal/DAO/OdontologoDAO.java
+++ proyectoFinal/src/main/java/com/digitalhouse/proyectoFinal/DAO/OdontologoDAO.java
@@ -1,4 +1,14 @@
 package com.digitalhouse.proyectoFinal.DAO;
 
+import com.digitalhouse.proyectoFinal.Servicio.OdontologoService;
+
+import java.util.List;
+import java.util.logging.Logger;
+
 public interface OdontologoDAO {
+    public Boolean crearOdontologo();
+    public Boolean listarTodos();
+    public List<> listarOdontologos();
+    public Boolean actualizarOdontologo();
+    public Boolean borrarOdontologo();
 }
