ce4a369b536a2697bb60b195b074fdf5d91a86d6
FJCon <conilfernando@gmail.com>
2023-08-23 20:26:25 -0300

    ModeloDAO devuelve generics

--- proyectoFinal/src/main/java/com/digitalhouse/proyectoFinal/DAO/ModeloDAO.java
+++ proyectoFinal/src/main/java/com/digitalhouse/proyectoFinal/DAO/ModeloDAO.java
@@ -3,9 +3,9 @@ package com.digitalhouse.proyectoFinal.DAO;
 import java.util.List;
 
 public interface ModeloDAO <T>{
-    public Boolean guardar(T t);
+    public T guardar(T t);
     public List<T> listarTodos();
-    public Object listar(T t);
+    public T listar(T t);
     public Boolean actualizar(T t);
     public Boolean borrar(T t);
 }
