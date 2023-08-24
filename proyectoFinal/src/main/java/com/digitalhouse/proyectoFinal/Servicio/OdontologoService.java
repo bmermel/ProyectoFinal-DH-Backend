3b4aba9b0ed4c2ab5740d5430e855319684ef902
FJCon <conilfernando@gmail.com>
2023-08-23 21:50:04 -0300

    Odontologo service creado y estructurado

--- proyectoFinal/src/main/java/com/digitalhouse/proyectoFinal/Servicio/OdontologoService.java
+++ proyectoFinal/src/main/java/com/digitalhouse/proyectoFinal/Servicio/OdontologoService.java
@@ -1,4 +1,30 @@
 package com.digitalhouse.proyectoFinal.Servicio;
 
+import com.digitalhouse.proyectoFinal.DAO.ModeloDAO;
+import com.digitalhouse.proyectoFinal.Modelo.Odontologo;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public class OdontologoService {
+    private ModeloDAO<Odontologo> odontolodoDAO;
+    public void setOdontolodoDAO(ModeloDAO<Odontologo> odontolodoDAO){
+        this.odontolodoDAO = odontolodoDAO;
+    }
+    public void guardar(Odontologo odontologo){
+        odontolodoDAO.guardar(odontologo);
+    }
+
+    public List<Odontologo> listarTodos(){
+        return odontolodoDAO.listarTodos();
+    }
+    public Odontologo listar(Odontologo odontologo) throws ClassNotFoundException, SQLException{
+        return odontolodoDAO.listar(odontologo);
+    }
+    public void actualizar(Odontologo odontologo) throws SQLException, ClassNotFoundException {
+        odontolodoDAO.listar(odontologo);
+    }
+    public void borrar(Odontologo odontologo) throws ClassNotFoundException, SQLException{
+        odontolodoDAO.borrar(odontologo);
+    }
 }
