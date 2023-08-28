7e0b87430be6987258a285b6f3e52f13e501b948
Brian Mermel <bmermel@gmail.com>
2023-08-28 20:53:25 -0300

    DTO de paciente y domicilio done

--- proyectoFinal/src/main/java/com/digitalhouse/proyectoFinal/DTO/DomicilioDTO.java
+++ proyectoFinal/src/main/java/com/digitalhouse/proyectoFinal/DTO/DomicilioDTO.java
@@ -1,4 +1,31 @@
 package com.digitalhouse.proyectoFinal.DTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class DomicilioDTO {
+    private Integer id;
+    private String calle;
+    private String numero;
+    private String localidad;
+    private String provincia;
+
+
+
+    @Override
+    public String toString() {
+        return "ActualizarDomicilioDto{" +
+                "id=" + id +
+                ", calle='" + calle + '\'' +
+                ", numero='" + numero + '\'' +
+                ", localidad='" + localidad + '\'' +
+                ", provincia='" + provincia + '\'' +
+                '}';
+    }
 }
--- proyectoFinal/src/main/java/com/digitalhouse/proyectoFinal/DTO/PacienteDTO.java
+++ proyectoFinal/src/main/java/com/digitalhouse/proyectoFinal/DTO/PacienteDTO.java
@@ -1,4 +1,37 @@
 package com.digitalhouse.proyectoFinal.DTO;
 
+import com.digitalhouse.proyectoFinal.Entity.Domicilio;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class PacienteDTO {
+    private Integer id;
+    private String nombre;
+    private String apellido;
+    private String dni;
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date fechaAlta;
+    private Domicilio domicilio;
+
+    @Override
+    public String toString() {
+        return "ActualizarPacienteDto{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", dni='" + dni + '\'' +
+                ", fechaAlta=" + fechaAlta +
+                ", domicilio=" + domicilio +
+                '}';
+    }
+
 }
