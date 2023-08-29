fb740e35bfee9e8758d44fecda4d42d392dde2df
FJCon <conilfernando@gmail.com>
2023-08-29 19:47:12 -0300

    Agrego Jackson a pom.xml

--- proyectoFinal/src/main/java/com/digitalhouse/proyectoFinal/Controller/TurnoController.java
+++ proyectoFinal/src/main/java/com/digitalhouse/proyectoFinal/Controller/TurnoController.java
@@ -1,4 +1,9 @@
 package com.digitalhouse.proyectoFinal.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RestController("/Turnos")
 public class TurnoController {
 }
