49543a38b41e1391e8cd54415889361a20d34771
FJCon <conilfernando@gmail.com>
2023-08-29 20:57:19 -0300

    OdontologoController

--- proyectoFinal/src/main/java/com/digitalhouse/proyectoFinal/Controller/OdontologoController.java
+++ proyectoFinal/src/main/java/com/digitalhouse/proyectoFinal/Controller/OdontologoController.java
@@ -1,12 +1,5 @@
 package com.digitalhouse.proyectoFinal.Controller;
 
-<<<<<<< HEAD
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RestController;
-
-@Controller
-@RestController("/Odontologos")
-=======
 import com.digitalhouse.proyectoFinal.DTO.OdontologoDTO;
 import com.digitalhouse.proyectoFinal.Services.OdontologoService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RestController("/odontologos")
->>>>>>> e8a58f62ef6ae4962902090b61c2d6da7af5f3a7
 public class OdontologoController {
     @Autowired
     private OdontologoService service;
