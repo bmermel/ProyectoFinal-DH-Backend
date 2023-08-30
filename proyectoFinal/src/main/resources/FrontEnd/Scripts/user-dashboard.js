e16e7ab4ecac8ca85b7e634120a34fab01bffeea
FJCon <conilfernando@gmail.com>
2023-08-29 22:33:46 -0300

    testeo clicks en botones de user-dashboard.html

--- proyectoFinal/src/main/resources/FrontEnd/Scripts/user-dashboard.js
+++ proyectoFinal/src/main/resources/FrontEnd/Scripts/user-dashboard.js
@@ -2,12 +2,16 @@ window.addEventListener("load", function(){
 
     const btnNuevoTurno = document.querySelector('#nuevo-turno');
     const btnCancelar = document.querySelector('#cancelar-turno');
+    const btnVerTurnos = document.querySelector('#ver-todos')
 
     btnNuevoTurno.addEventListener("click",()=>{
         console.log("Crear un nuevo turno");
     })
     btnCancelar.addEventListener("click",()=>{
         console.log("Cancelar un turno");
+    })
+    btnVerTurnos.addEventListener("click",()=>{
+        console.log("Listar todos los turnos");
     })
 
 
