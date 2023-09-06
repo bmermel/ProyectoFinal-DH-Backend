c2a396150dd84d5a14d8e66f311b684410ca982f
FJCon <conilfernando@gmail.com>
2023-09-06 20:22:16 -0300

    static

--- proyectoFinal/src/main/java/com/digitalhouse/proyectoFinal/Controller/HomeController.java
+++ proyectoFinal/src/main/java/com/digitalhouse/proyectoFinal/Controller/HomeController.java
@@ -1,14 +1,14 @@
 package com.digitalhouse.proyectoFinal.Controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/")
-public class VistaController {
+@Controller
+public class HomeController {
     @GetMapping
     public String login(){
-        return "./login.html";
+        return "login.html";
     }
 }
