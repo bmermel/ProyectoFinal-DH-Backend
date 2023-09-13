00a5e99d88f5717a10591982843911767bd8f0e9
Brian Mermel <bmermel@gmail.com>
2023-09-12 21:40:59 -0300

    ahora si

--- proyectoFinal/src/main/java/com/digitalhouse/proyectoFinal/Auth/AuthService.java
+++ proyectoFinal/src/main/java/com/digitalhouse/proyectoFinal/Auth/AuthService.java
@@ -5,6 +5,10 @@ import com.digitalhouse.proyectoFinal.User.Role;
 import com.digitalhouse.proyectoFinal.User.User;
 import com.digitalhouse.proyectoFinal.User.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,13 +16,21 @@ import org.springframework.stereotype.Service;
 public class AuthService {
     private final UserRepository userRepository;
     private final JwtService jwtService;
-    public AuthResponse login(LoginRequest request){
-        return null;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    public AuthResponse login(LoginRequest request) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
+        UserDetails user=userRepository.findByUsuario(request.getUsername()).orElseThrow();
+        String token=jwtService.getToken(user);
+        return AuthResponse.builder()
+                .token(token)
+                .build();
+
     }
     public AuthResponse register(RegisterRequest request){
         User user = User.builder()
                 .usuario(request.getUsername())
-                .contrasena((request.getPassword()))
+                .contrasena(passwordEncoder.encode(request.getPassword()))
                 .email((request.getEmail()))
                 .role(Role.USER)
                 .build();
