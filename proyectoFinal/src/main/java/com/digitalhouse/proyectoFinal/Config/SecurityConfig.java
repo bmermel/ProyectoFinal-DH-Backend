b670e9a46eb9b62574d98d745b4a424fb82a93d4
FJCon <conilfernando@gmail.com>
2023-09-12 22:17:46 -0300

    error register

--- proyectoFinal/src/main/java/com/digitalhouse/proyectoFinal/Config/SecurityConfig.java
+++ proyectoFinal/src/main/java/com/digitalhouse/proyectoFinal/Config/SecurityConfig.java
@@ -1,18 +1,18 @@
 package com.digitalhouse.proyectoFinal.Config;
 
 import com.digitalhouse.proyectoFinal.Jwt.JwtAuthenticationFilter;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
-import org.springframework.security.web.util.matcher.RequestMatcher;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -25,16 +25,15 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
     {
-
-
         return http
                 .csrf(csrf ->
                         csrf
                                 .disable())
                 .authorizeHttpRequests(authRequest ->
                         authRequest
-
-                                .requestMatchers("/auth/**").permitAll()
+                                .requestMatchers(new AntPathRequestMatcher("/auth/login")).permitAll()
+                                .requestMatchers(new AntPathRequestMatcher("/auth/register")).permitAll()
+                                .requestMatchers(new AntPathRequestMatcher("/login.html")).permitAll()
                                 .anyRequest().authenticated()
                 )
                 .sessionManagement(sessionManager->
